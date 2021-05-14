8e2400b5dffcbd19363e887f4da0d99bf0b3137d
claudiocds1987 <cla8787@gmail.com>
2021-05-14 14:52:18 -0300

    iconos y background en thead

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import {
-  Navbar,
-  Nav,
-  Form,
-  FormControl,
-  Button,
-  Container,
-} from "react-bootstrap";
+  faHome,
+  faPlusCircle,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 // npm install react-router-dom
 import { Link } from "react-router-dom";
@@ -16,31 +17,23 @@ const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Link to="/home" className="navbar-brand">Operation App</Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Link className="nav-link" to="/home">
-              Home
-            </Link>
-            <Link className="nav-link" to="/createOperation">
-              New Operation
-            </Link>
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </Nav>
-          <Form>
-            <div className="d-flex">
-              <div>
-                <FormControl type="text" placeholder="Search" />
-              </div>
-              <div>
-                <Button variant="outline-success">Search</Button>
-              </div>
-            </div>
-          </Form>
-        </Navbar.Collapse>
+          <Link to="/home" className="navbar-brand">
+            Operations App
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Link className="nav-link" to="/home">
+                <FontAwesomeIcon icon={faHome} /> Home
+              </Link>
+              <Link className="nav-link" to="/createOperation">
+                <FontAwesomeIcon icon={faPlusCircle} /> New Operation
+              </Link>
+              <Link className="nav-link" to="/login">
+                <FontAwesomeIcon icon={faUser} /> Login
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
       </Container>
     </Navbar>
   );
